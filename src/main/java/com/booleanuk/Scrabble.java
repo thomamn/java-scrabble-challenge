650db73b93733367039227224f5c5860ee00e8ea
TMajlu <thomasmajnie@gmail.com>
2024-08-13 16:00:47 +0200

    Did the core exercise and the extension.

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -26,17 +26,103 @@ public class Scrabble {
         letterValues.put("JX", 8);
         letterValues.put("QZ", 10);
         String newWord=sWord.toUpperCase();
-        for (char c: newWord.toCharArray()){
-            for (String key: letterValues.keySet()){
-                for (char d: key.toCharArray()){
-                    if(c==d){
-                        score+=letterValues.get(key);
+        char[] charWord=newWord.toCharArray();
+        int multi=1;
+        int terminate=1;
+        int multiWord=1;
+
+        if (charWord[0]=='{' & charWord[2]!='}'){
+            if (charWord[charWord.length-1]=='}'){
+                multiWord=2;
+            }
+            if (charWord[1]=='[' & charWord[charWord.length-2]==']'){
+                multiWord=6;
+            }
+        }
+
+        if (charWord[0]=='[' & charWord[2]!=']'){
+            if (charWord[charWord.length-1]==']'){
+                multiWord=3;
+            }
+        }
+        for (int i=0; i<charWord.length; ++i){
+            if (charWord[i]=='!' || charWord[i]=='|'){
+                terminate=0;
+            }
+            if(charWord[i]=='{'){
+                if(charWord[i+2]=='}'){
+                    multi=2;
+                }
+                else if (charWord[i+2]!='}'){
+                    if (multiWord==1) {
+                        multi=0;
+                        terminate=0;
+                    }
+
+
+
+                }
+                continue;
+
+            }
+            else if(charWord[i]=='['){
+                if(charWord[i+2]==']'){
+                    multi=3;
+                }
+                else if (charWord[i+2]!=']'){
+                    if (multiWord==1) {
+                        multi=0;
+                        terminate=0;
+                    }
+
+
+
+                }
+                continue;
+
+            }
+
+
+            else if(charWord[i]=='}'){
+                if(multi==2){
+                    multi=1;
+                    continue;
+                }
+                else if (multi!=2){
+                    if (multiWord==1) {
+                        terminate = 0;
+                    }
+                }
+
+            }
+
+            else if(charWord[i]==']'){
+                if(multi==3){
+                    multi=1;
+                    continue;
+                }
+                else if (multi!=3){
+                    if (multiWord==1) {
+                        terminate = 0;
+                    }
+                }
+
+            }
+
+
+            for (String key : letterValues.keySet()) {
+                for (char d : key.toCharArray()) {
+                    if (charWord[i] == d) {
+                        score += letterValues.get(key) * multi;
+                        continue;
 
                     }
                 }
             }
+
+
         }
-        return score;
+        return multiWord*score*terminate;
     }
 
 }

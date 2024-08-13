f351fb0c3ecc92ffc38c1f40d5ce9866c457bedc
TMajlu <thomasmajnie@gmail.com>
2024-08-13 14:49:26 +0200

    Did the core exercise.

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,42 @@
 package com.booleanuk;
 
-public class Scrabble {
-    public Scrabble(String word) {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class Scrabble {
+    String sWord;
+    public Scrabble(String word) {
+        sWord=word;
     }
 
+    HashMap<String, Integer> letterValues=new HashMap<String, Integer>(
+    );
+
+
+
+
     public int score() {
-        return -1;
+        int score=0;
+        letterValues.put("AEIOULNRST", 1);
+        letterValues.put("DG", 2);
+        letterValues.put("BCMP", 3);
+        letterValues.put("FHVWY", 4);
+        letterValues.put("K", 5);
+        letterValues.put("JX", 8);
+        letterValues.put("QZ", 10);
+        String newWord=sWord.toUpperCase();
+        for (char c: newWord.toCharArray()){
+            for (String key: letterValues.keySet()){
+                for (char d: key.toCharArray()){
+                    if(c==d){
+                        score+=letterValues.get(key);
+
+                    }
+                }
+            }
+        }
+        return score;
     }
 
 }
